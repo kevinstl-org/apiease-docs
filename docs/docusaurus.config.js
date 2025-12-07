d15d1f3add99f4ba0d72e70c445385e10d3f07f9
kevinstl <kjwilde@gmail.com>
2025-12-06 22:10:28 -0600

    Update tagline.

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -11,7 +11,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'APIEase',
-  tagline: 'Secure automation for Shopify teams',
+  tagline: 'The API Integration And Automation Platform',
   favicon: 'img/favicon.ico',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
